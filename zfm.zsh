1ad046de915ae05179ee6c031abe024c28a0bac9
Pablo Arias <pabloariasal@gmail.com>
2021-10-12 23:27:26 +0200

    remove cluttering comments

--- zfm.zsh
+++ zfm.zsh
@@ -125,38 +125,25 @@ ZFM_NO_BINDINGS                         Disabled creation of bindings
 ZFM_BOOKMARKS_FILE                      Bookmarks file. Defaults to '~/.zfm.txt'
 "
 
-# CHANGE: Added logic to it's own funnction
-function set_bookmarks_file() 
+function set_bookmarks_file()
 {
-    # CHANGE: If ZFM_BOOKMARKS_FILE is set then use that, otherwise fall back to the default location
-        # Switched to more robust POSIX compliant test 
     [[ -n "${ZFM_BOOKMARKS_FILE+x}" ]] && local bookmarks_file="$ZFM_BOOKMARKS_FILE" || local bookmarks_file="${HOME}/.zfm.txt"
-    
-    # CHANGE: Redirect stdout stderr so we can print a nicer error. 
+
     if [ ! -e "$bookmarks_file" ]; then
         touch "$bookmarks_file" &> /dev/null
     fi
-    
-    #CHANGE: Check if the bookmark file exists, if not give likely reasons as to why. We could ask the user where they want it and then ask them to add it or have the program append it to their zshrc.
+
     if [ ! -f "$bookmarks_file" ]; then
         printf "ZFM failed to create a bookmarks file.\n\n ZFM_BOOKMARKS_FILE is currently set to '${ZFM_BOOKMARKS_FILE}' if the ZFM_BOOKMARKS_FILE is variable has been set it has likely not been done correctly, the path does not exists, the variable is not exported, or the zfm cannot access the set directory. See zfm --help for more information.\n"
         return 1
     fi
-    
-    # "Return" the value
-    echo $bookmarks_file
 
+    echo $bookmarks_file
 }
 
 function zfm()
-{   
-    
+{
     bookmarks_file=$(set_bookmarks_file)
-
-    # NOTE: Maybe we should switch this to GETOPTS, which has support for 
-        # Flags and Params like `zfm -a .dots`but does not support long options 
-        # like --help or --dirs
-
     case "$1" in
         'list')
             __zfm_check_regex "$1" '(--files|--dirs)' "${@:2}" || return 1
@@ -180,7 +167,6 @@ function zfm()
             fi
             ;;
         'add')
-            # CHANGE: In case you are using multiple files, show where it's being added
             echo "Added to: $bookmarks_file"
             __zfm_add_items_to_file "$bookmarks_file" "${@:2}" || return 1
             ;;
@@ -206,7 +192,6 @@ function zfm()
             ! [[  -z "${@:2}" ]] && echo "Invalid option '${@:2}' for '$1'" && return 1
             ${EDITOR:-vim} "$bookmarks_file"
             ;;
-        # CHANGE: Added Help Option
         'help')
             echo "$usage" >&2
             ;;
